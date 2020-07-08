58f60163641ee48974e0ce1182a74f97147cd411
ME ADMIN <ME@MEs-MacBook-Pro.local>
2020-07-08 20:59:24 +0200

    second commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,17 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/juanSainzWeb/gatsby-starter-forty/Juan/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/juanSainzWeb/gatsby-starter-forty/Juan/src/pages/404.js"))),
+  "component---src-pages-elements-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/juanSainzWeb/gatsby-starter-forty/Juan/src/pages/elements.js"))),
+  "component---src-pages-generic-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/juanSainzWeb/gatsby-starter-forty/Juan/src/pages/generic.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/juanSainzWeb/gatsby-starter-forty/Juan/src/pages/index.js"))),
+  "component---src-pages-landing-0-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/juanSainzWeb/gatsby-starter-forty/Juan/src/pages/landing0.js"))),
+  "component---src-pages-landing-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/juanSainzWeb/gatsby-starter-forty/Juan/src/pages/landing.js"))),
+  "component---src-pages-landing-one-js": hot(preferDefault(require("/Users/ME/Desktop/WORK/REACT_work/juanSainzWeb/gatsby-starter-forty/Juan/src/pages/landingOne.js")))
+}
+
